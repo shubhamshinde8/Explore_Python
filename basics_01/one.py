657ab43eb1de812060e102643f48371b796a3ee8
Shubham Shinde <shubhamshinde8746@gmail.com>
2025-08-17 04:54:50 -1200

    exploring python

---
+++ basics_01/one.py
@@ -0,0 +1,9 @@
+name="shubham shinde"
+mail="shubhamshinde8746@gmail.com"
+password=2387876
+
+print("Name : ",name)
+print("Email : ",mail)
+print("password : ",password)
+
+
---
+++ basics_01/sum.py
@@ -0,0 +1,28 @@
+
+
+
+# name = "Shubham"   # str
+# age = 22           # int
+# height = 1.74      # float (meters)
+# is_student = True  # bool
+
+# print(type(name), type(age), type(height), type(is_student))
+
+
+# comments in python 
+
+# numebers = 123456
+# print(numebers)
+
+# name="Shubham"
+# print(name)
+
+# name="Shinde"
+# print(name)
+
+
+# myList=[1,2,3,4,5]
+# print(myList[0])
+# myList[0]=20
+
+# print(myList)
